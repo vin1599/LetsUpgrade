8dd5a5a325d75062c7b602e8c9e1712de5add5fd
vin1599 <53434367+vin1599@users.noreply.github.com>
2020-04-10 16:42:08 +0530

    Create day2.py

---
+++ Day 2/day2.py
@@ -0,0 +1,34 @@
+# Name :- vinayak parab
+
+a = 3.14
+print(type(a))
+print(type(int(a)))
+
+str = "vinayak parab"
+print(str)
+print(str[2])
+print(str[-2])
+print(str[-8:-5])
+
+# pemdas :- parenthesis, exponents, multiplication, division, addition, subtraction.
+pemdas = 2*int(3)*4*int(3**2)+13-4
+print(pemdas)
+
+# list and tuple
+# list are mutable and tuple is not mutable
+#list represent in sqr brackets []
+#tuple represent in round brackets ()
+
+list1 = [[12, 16], 1, 18, 'vinayak', 'parab']
+print(list1)
+print(list1[0])
+print(list1[0][1])
+list1[3] = 999
+print(list1)
+
+tuple = (1,2,3,4)
+print(tuple)
+"tuple[0] = 999     #its give error because tuple its not mutable"
+stre = 'hello everyone'
+print(stre[0:6])
+print(stre[-8:])
